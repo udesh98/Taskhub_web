2fe66d4a1011cd0c574835f0bff4500c738ea9ba
udesh98 <um4me.98@gmail.com>
2022-01-26 19:54:13 +0530

    Modified customer sidebar

--- views/Customer/customer_sidebar.php
+++ views/Customer/customer_sidebar.php
@@ -1,13 +1,13 @@
 <?php ?>
 <div class="sidebar">
     <ul>
-        <li class="top"><a href="<?php echo fullURLfront; ?>/Customer/customer_service"><i class="fa fa-search"></i> Services</a></li>
-        <li><a href="<?php echo fullURLfront; ?>/Customer/customer_profile"><i class="fa fa-user"></i> Profile</a></li>
-        <li><a href="<?php echo fullURLfront; ?>/Customer/customer_calender"><i class="fa fa-calendar"></i> Booking</a></li>
-        <li><a href="<?php echo fullURLfront; ?>/Customer/customer_complaint"><i class="fa fa-frown-o"></i> Complaints</a></li>
-        <li><a href="<?php echo fullURLfront; ?>/Customer/customer_history"><i class="fa fa-history"></i> History</a></li>
-        <li><a href="<?php echo fullURLfront; ?>/Customer/customer_help"><i class="fa fa-info-circle"></i> Help</a></li>
-        <li><a href="<?php echo fullURLfront; ?>/Customer/customer_viewad"><i class="fa fa-audio-description"></i> View Advertsiment</a></li>
-        <li><a href="<?php echo fullURLfront; ?>/Customer/customer_postad"><i class="fa fa-audio-description"></i> Post Advertsiment</a></li>
+        <li class="top"><a href="<?php echo fullURLfront; ?>/Customer/customer_service" class="<?php echo (empty($_SESSION['loggedin'])) ? 'disabled-link' : ''; ?>"><i class="fa fa-search"></i> Services</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Customer/customer_profile" class="<?php echo (empty($_SESSION['loggedin'])) ? 'disabled-link' : ''; ?>"><i class="fa fa-user"></i> Profile</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Customer/customer_calender" class="<?php echo (empty($_SESSION['loggedin'])) ? 'disabled-link' : ''; ?>"><i class="fa fa-calendar"></i> Booking</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Customer/customer_complaint" class="<?php echo (empty($_SESSION['loggedin'])) ? 'disabled-link' : ''; ?>"><i class="fa fa-frown-o"></i> Complaints</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Customer/customer_history" class="<?php echo (empty($_SESSION['loggedin'])) ? 'disabled-link' : ''; ?>"><i class="fa fa-history"></i> History</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Customer/customer_help" class="<?php echo (empty($_SESSION['loggedin'])) ? 'disabled-link' : ''; ?>"><i class="fa fa-info-circle"></i> Help</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Customer/customer_viewad" class="<?php echo (empty($_SESSION['loggedin'])) ? 'disabled-link' : ''; ?>"><i class="fa fa-audio-description"></i> View Advertsiment</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Customer/customer_postad" class="<?php echo (empty($_SESSION['loggedin'])) ? 'disabled-link' : ''; ?>"><i class="fa fa-audio-description"></i> Post Advertsiment</a></li>
     </ul>
 </div>
